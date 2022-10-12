d6aadd80410c7e5b7ebdd4c5d8c93f2baf9cd8da
Max1203sr <maksimsr312@gmail.com>
2022-10-12 14:42:10 +0300

    Version 1.0.0.1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿class Task
+{
+    void centimeters() // From meters to centimeters
+    {
+        Console.WriteLine("From meters to centimeters:\n");
+
+        Console.WriteLine("Enter meters");
+        int meters = int.Parse(Console.ReadLine());
+        int centimeters = meters * 100;
+
+        Console.WriteLine("meters to centimeters: " + centimeters);
+    }
+
+    void kilometers() // From meters to km
+    {
+        Console.WriteLine("From meters to km:\n");
+
+        Console.WriteLine("Enter meters");
+        int meters = int.Parse(Console.ReadLine());
+        double kilometers = (double)meters / 1000;
+
+        Console.WriteLine("meters to kilometers: " + kilometers);
+    }
+
+    static void Main(string[] args)
+    {
+        Task task = new Task();
+        task.centimeters();
+        task.kilometers();
+    }
+}
